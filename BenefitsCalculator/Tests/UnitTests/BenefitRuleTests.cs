20ed5da70dc77183b47f1ff7361597d6b9d0dac4
Kelley Crutcher <kcrutch@outlook.com>
2018-07-08 16:21:52 -0400

    + adding a unit test project

---
+++ BenefitsCalculator/Tests/UnitTests/BenefitRuleTests.cs
@@ -0,0 +1,45 @@
+﻿using BenefitsCalculator.Common.Entities;
+using BenefitsCalculator.Components.Rules;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class BenefitRuleTests
+    {
+        private BeneficiaryNameDiscountRule _rule;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this._rule = new BeneficiaryNameDiscountRule();
+        }
+
+        [TestMethod]
+        public void NameDiscountShouldApply()
+        {
+            decimal grossBenefitCost = 1000;
+
+            var benefit = BuildBenefitForTest(grossBenefitCost, "Aaron");
+
+            decimal discount = _rule.DetermineDiscount(benefit);
+
+            Assert.AreEqual(grossBenefitCost * BeneficiaryNameDiscountRule.DiscountAmount, discount);
+        }
+
+        [TestMethod]
+        public void NameDiscountShouldNotApply()
+        {
+            var benefit = BuildBenefitForTest(1000, "James");
+
+            decimal discount = _rule.DetermineDiscount(benefit);
+
+            Assert.AreEqual(0, discount);
+        }
+
+        private Benefit BuildBenefitForTest(decimal grossBenefitCost, string beneficiaryFirstName)
+        {
+            return new Benefit() { GrossCost = grossBenefitCost, Beneficiary = new Person() { FirstName = beneficiaryFirstName } };
+        }
+    }
+}
