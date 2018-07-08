d941c39a8e0c4017f0778512fc1022653b4014c2
Kelley Crutcher <kcrutch@outlook.com>
2018-07-08 19:37:07 -0400

    + adding a controller for payroll settings

---
+++ BenefitsCalculator/BenefitsCalculator.WebApi/Controllers/PayrollSettingsController.cs
@@ -0,0 +1,27 @@
+﻿using BenefitsCalculator.Common.Entities.Configuration;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace BenefitsCalculator.WebApi.Controllers
+{
+    [EnableCors("OpenPolicy")]
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class PayrollSettingsController : Controller
+    {
+        private IOptions<PayrollSettings> _payrollSettings;
+
+        public PayrollSettingsController(IOptions<PayrollSettings> payrollSettings)
+        {
+            this._payrollSettings = payrollSettings;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(this._payrollSettings.Value);
+        }
+    }
+}
